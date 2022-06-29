d5d586b5757260e28e09ed7bba87ad5a88b67940
Diego Saavedra <dsaavedra88@gmail.com>
2022-06-28 21:56:34 -0500

    Templates y Vistas

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Curso
 
 def hompage(request):
-    return HttpResponse("Hola bunas nochs")
+    #return HttpResponse("Hola buenas noches")
+    return render(request, "main/inicio.html", {"cursos": Curso.objects.all})
